f20c3256acd40064a198fb22b549a76bd9bcd3b8
Glayder <glayder.silva@idvlab.com.br>
2021-03-19 09:21:08 -0300

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './assets/images/global.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CooperSystem</h1>
+    </div>
+  );
+}
+
+export default App;
